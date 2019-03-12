71919748ddf7643a7fc9d6aef87b3d53f3c49ee9
nagarjun <nagarjun_nagesh@outlook.com>
2019-03-12 15:50:31 +0100

    implemented springboot

---
+++ EveryRupee/src/main/java/in/co/everyrupee/everyrupeespringboot/EveryRupeeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package in.co.everyrupee.everyrupeespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryRupeeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryRupeeSpringBootApplication.class, args);
+	}
+
+}
