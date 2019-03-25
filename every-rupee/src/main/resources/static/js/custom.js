0bf565b5d41babd9c7b767d65dca0144fb6e3606
nagarjun <nagarjun_nagesh@outlook.com>
2019-03-25 17:34:30 +0100

    Remove unnecessary java script

--- every-rupee/src/main/resources/static/js/custom.js
+++ every-rupee/src/main/resources/static/js/custom.js
@@ -1,23 +1,11 @@
 /**
  * Custom Java script for EveryRupee
  * 
- * https://www.foreach.be/blog/spring-social-how-render-social-login-flow-popup
+ * https://github.com/ccoloradoc/OAuth2Sample
  * 
  * Nagarjun Nagesh
  */
 
 $(document).ready(function() {
 	
-	function loginSocial( e ) {
-	    e.preventDefault();
-	    var triggerElement = $( e.target );
-	    var __ret = calculatePopupDimensions(); 
-	
-	    var provider = triggerElement.attr( 'data-social-provider' );
-	    var url = '/spring-social-quickstart/signin/' + provider + '/popup';
-	    var specs = 'scrollbars=yes, width=' + __ret.w 
-	        + ', height=' + __ret.h + ', top=' + __ret.top + ', left=' + __ret.left;
-	 
-	   window.open( url, '_blank', specs );
-	} 
 });
