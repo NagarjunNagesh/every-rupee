cb600cc016663cb904201d1a89c2086cc5e2e9de
nagarjun <nagarjun_nagesh@outlook.com>
2019-04-15 17:42:25 +0200

    Changes to add profile page to the dashboard

--- every-rupee/src/main/java/in/co/everyrupee/controller/DashboardController.java
+++ every-rupee/src/main/java/in/co/everyrupee/controller/DashboardController.java
@@ -11,42 +11,49 @@ import in.co.everyrupee.constants.GenericConstants;
 @Controller
 public class DashboardController {
 
-	@Autowired
-	public DashboardController(Environment environment) {
-	}
+    @Autowired
+    public DashboardController(Environment environment) {
+    }
 
-	@RequestMapping(GenericConstants.DASHBOARD_INCOME_URL)
-	public ModelAndView dashboardIncome() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName(GenericConstants.DASHBOARD_INCOME_VIEW_NAME_OBJECT);
-		return modelAndView;
-	}
+    @RequestMapping(GenericConstants.DASHBOARD_INCOME_URL)
+    public ModelAndView dashboardIncome() {
+	ModelAndView modelAndView = new ModelAndView();
+	modelAndView.setViewName(GenericConstants.DASHBOARD_INCOME_VIEW_NAME_OBJECT);
+	return modelAndView;
+    }
 
-	@RequestMapping(GenericConstants.DASHBOARD_SAVINGS_URL)
-	public ModelAndView dashboardSavings() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName(GenericConstants.DASHBOARD_SAVINGS_VIEW_NAME_OBJECT);
-		return modelAndView;
-	}
+    @RequestMapping(GenericConstants.DASHBOARD_SAVINGS_URL)
+    public ModelAndView dashboardSavings() {
+	ModelAndView modelAndView = new ModelAndView();
+	modelAndView.setViewName(GenericConstants.DASHBOARD_SAVINGS_VIEW_NAME_OBJECT);
+	return modelAndView;
+    }
 
-	@RequestMapping(GenericConstants.DASHBOARD_DEBT_URL)
-	public ModelAndView dashboardDebt() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName(GenericConstants.DASHBOARD_DEBT_VIEW_NAME_OBJECT);
-		return modelAndView;
-	}
+    @RequestMapping(GenericConstants.DASHBOARD_DEBT_URL)
+    public ModelAndView dashboardDebt() {
+	ModelAndView modelAndView = new ModelAndView();
+	modelAndView.setViewName(GenericConstants.DASHBOARD_DEBT_VIEW_NAME_OBJECT);
+	return modelAndView;
+    }
 
-	@RequestMapping(GenericConstants.DASHBOARD_INVESTMENT_URL)
-	public ModelAndView dashboardInvestment() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName(GenericConstants.DASHBOARD_INVESTMENT_VIEW_NAME_OBJECT);
-		return modelAndView;
-	}
+    @RequestMapping(GenericConstants.DASHBOARD_INVESTMENT_URL)
+    public ModelAndView dashboardInvestment() {
+	ModelAndView modelAndView = new ModelAndView();
+	modelAndView.setViewName(GenericConstants.DASHBOARD_INVESTMENT_VIEW_NAME_OBJECT);
+	return modelAndView;
+    }
 
-	@RequestMapping(GenericConstants.DASHBOARD_SETTINGS_URL)
-	public ModelAndView dashboardSettings() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName(GenericConstants.DASHBOARD_SETTINGS_VIEW_NAME_OBJECT);
-		return modelAndView;
-	}
+    @RequestMapping(GenericConstants.DASHBOARD_SETTINGS_URL)
+    public ModelAndView dashboardSettings() {
+	ModelAndView modelAndView = new ModelAndView();
+	modelAndView.setViewName(GenericConstants.DASHBOARD_SETTINGS_VIEW_NAME_OBJECT);
+	return modelAndView;
+    }
+
+    @RequestMapping(GenericConstants.DASHBOARD_PROFILE_URL)
+    public ModelAndView dashboardProfile() {
+	ModelAndView modelAndView = new ModelAndView();
+	modelAndView.setViewName(GenericConstants.DASHBOARD_PROFILE_VIEW_NAME_OBJECT);
+	return modelAndView;
+    }
 }
