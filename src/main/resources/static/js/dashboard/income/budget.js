272dbaa1095eb97651743cab56eb66eea231b789
nagarjun <nagarjun@nagarjun-HP-Pavilion-g6-Notebook-PC>
2019-07-14 22:09:43 +0200

    Build a pie chart for budget tab

--- src/main/resources/static/js/dashboard/income/budget.js
+++ src/main/resources/static/js/dashboard/income/budget.js
@@ -5,12 +5,11 @@ $(document).ready(function(){
 	const currentCurrencyPreference = document.getElementById('currentCurrencySymbol').innerText;
 	// User Budget Map Cache
 	let userBudgetCache = {};
+	// User transaction category ID and total
+	let categoryTotalMapCache = {};
 	
 	// Fetch user budget and build the div
 	fetchAllUserBudget();
-	
-	// Fetch user transactions
-	fetchTransactions();
 	
 	// Fetches all the user budget and displays them in the user budget
 	function fetchAllUserBudget() {
@@ -36,6 +35,10 @@ $(document).ready(function(){
             	
             	// paints them to the budget dashboard
           	  	document.getElementById('budgetAmount').appendChild(budgetDivFragment);
+          	  	
+          	  	// Fetch user transactions
+          		fetchTransactions();
+          		
             }
 		});
 		
@@ -52,26 +55,23 @@ $(document).ready(function(){
 		let card = document.createElement("div");
 		card.classList = 'card';
 		
-		// Card Header
-		let cardHeader = document.createElement('div');
-		cardHeader.classList = 'card-header';
-		
-		// Card title with category name
-		let cardTitle = document.createElement('div');
-		cardTitle.classList = 'card-title';
-		cardTitle.innerText = categoryObject.categoryName;
-		cardHeader.appendChild(cardTitle);
-		card.appendChild(cardHeader);
-		
 		let cardBody = document.createElement("div");
 		cardBody.classList = 'card-body';
 		
 		// Card Row Remaining
 		let cardRowRemaining = document.createElement('div');
+		cardRowRemaining.classList = 'row';
+		
+		// Card title with category name
+		let cardTitle = document.createElement('div');
+		cardTitle.classList = 'col-lg-6 text-right';
+		cardTitle.innerText = categoryObject.categoryName;
+		cardRowRemaining.appendChild(cardTitle);
+		
 		
 		// <div id="budgetInfoLabelInModal" class="col-lg-12 text-right headingDiv justify-content-center align-self-center">Remaining (%)</div> 
 		let cardRemainingText = document.createElement('div');
-		cardRemainingText.classList = 'col-lg-12 text-right headingDiv justify-content-center align-self-center';
+		cardRemainingText.classList = 'col-lg-6 text-right headingDiv justify-content-center align-self-center';
 		cardRemainingText.id = 'budgetInfoLabelInModal-' + categoryObject.categoryId;
 		cardRemainingText.innerText = 'Remaining (%)';
 		cardRowRemaining.appendChild(cardRemainingText);
@@ -98,7 +98,7 @@ $(document).ready(function(){
 		// progress bar
 		let progressBar = document.createElement('div');
 		progressBar.id='progress-budget-' + categoryObject.categoryId;
-		progressBar.classList = 'progress-bar progress-bar-success-striped';
+		progressBar.classList = 'progress-bar progress-bar-budget-striped';
 		progressBar.setAttribute('role', 'progressbar');
 		progressBar.setAttribute('aria-valuenow', '0');
 		progressBar.setAttribute('aria-valuemin', '0');
@@ -110,7 +110,7 @@ $(document).ready(function(){
 		// Remaining Amount Div
 		let remainingAmountDiv = document.createElement('span');
 		remainingAmountDiv.id = 'remainingAmount-' + categoryObject.categoryId;
-		remainingAmountDiv.classList = 'mild-text-success';
+		remainingAmountDiv.classList = 'mild-text-budget';
 		
 		let currencyRemainingAmount = document.createElement('span');
 		currencyRemainingAmount.innerText = currentCurrencyPreference + '0.00';
@@ -129,6 +129,8 @@ $(document).ready(function(){
 			url: transactionAPIUrl + transactionFetchCategoryTotal + currentUser.financialPortfolioId + dateMeantFor + chosenDate + updateBudgetFalseParam,
             type: 'GET',
             success: function(categoryTotalMap) {
+            	// Store the result in a cache
+            	categoryTotalMapCache = categoryTotalMap;
             	// Get all the category id's
         		let categoryTotalKeys = Object.keys(categoryTotalMap);
         		for(let count = 0, length = categoryTotalKeys.length; count < length; count++){
@@ -180,12 +182,64 @@ $(document).ready(function(){
 	    				progressBarCategoryModal.setAttribute('aria-valuenow', 0);
 	    				progressBarCategoryModal.style.width = 0 + '%';
         			}
-    				
-        			
         			
         		}
+        		
+        		// Update the Budget Visualization module
+        		updateBudgetVisualization();
+        		
             }
 		});
+	}
+	
+	// Update the budget visualization module
+	function updateBudgetVisualization() {
+		let categoryTotalKeys = Object.keys(categoryTotalMapCache);
+		
+		let userBudgetCacheKeys = Object.keys(userBudgetCache);
+		
+		if(isNotEmpty(userBudgetCacheKeys)) {
+			let totalBudgetedCategoriesDiv = document.getElementById('totalBudgetedCategories');
+			totalBudgetedCategoriesDiv.innerText = userBudgetCacheKeys.length;
+			
+			if(isNotEmpty(categoryTotalKeys)) {
+				let toBeBudgetedDiv = document.getElementById('toBeBudgeted');
+				let toBeBudgetedAvailable = categoryTotalKeys.length - userBudgetCacheKeys.length;
+				toBeBudgetedDiv.innerText = toBeBudgetedAvailable;
+				
+				let userBudgetPercentage = round(((userBudgetCacheKeys.length / categoryTotalKeys.length) * 100),1);
+				let toBeBudgetedPercentage = round(((toBeBudgetedAvailable / categoryTotalKeys.length) * 100),1);
+				// labels: [Total Budgeted Category, To Be Budgeted]
+				let dataPreferences = {
+	                labels: [userBudgetPercentage + '%',toBeBudgetedPercentage + '%'],
+	                series: [userBudgetPercentage,toBeBudgetedPercentage]
+	            };
+				
+				buildPieChart(dataPreferences , 'chartBudgetVisualization');
+			}
+		}
+	}
+	
+	// Introduce Chartist pie chart
+	function buildPieChart(dataPreferences, id) {
+		 /*  **************** Public Preferences - Pie Chart ******************** */
+
+        var optionsPreferences = {
+		  donut: true,
+		  donutWidth: 50,
+		  donutSolid: true,
+		  startAngle: 270,
+		  showLabel: true,
+		  height: '230px'
+        };
+        
+        // Reset the chart
+        replaceHTML(id, '');
+        
+        if(isNotEmpty(dataPreferences)) {
+        	let transactionsChart = new Chartist.Pie('#' + id, dataPreferences, optionsPreferences);
+        }
+        
 	}
 	
 });
