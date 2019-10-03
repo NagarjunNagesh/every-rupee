766d4518a445032c68f5a1324803dca840b5557d
nagarjun <nagarjun@nagarjun-HP-Pavilion-g6-Notebook-PC>
2019-10-03 22:11:08 +0200

    View all accounts functionality

--- src/main/resources/static/js/dashboard/module/account.js
+++ src/main/resources/static/js/dashboard/module/account.js
@@ -519,21 +519,23 @@ $(document).ready(function(){
 	          url: BANK_ACCOUNT_CONSTANTS.bankAccountUrl + BANK_ACCOUNT_CONSTANTS.bankAccountCategorizeUrl,
 	          dataType: "json",
 	          success: function(categorizeBankAccount){
-	        	  console.log(categorizeBankAccount);
+	        	  let bAParentFrag = document.createDocumentFragment();
 	        	  let bAFragment = document.createDocumentFragment();
 		          // Fetch all the key set for the result
 	        	  let resultKeySet = Object.keys(categorizeBankAccount)
 	        	  for(let countGrouped = 0, lengthArray = resultKeySet.length; countGrouped < lengthArray; countGrouped++) {
 	        		  let key = resultKeySet[countGrouped];
 	             	  let value = categorizeBankAccount[key];
-	             	  bAFragment.appendChild(populateBAHead(value));
+	             	  bAFragment.appendChild(populateBAHead(key));
 	             	  let valueElementKeySet = Object.keys(value)
 	             	  for(let count = 0, length = valueElementKeySet.length; count < length; count++) {
 	             		  let subKey = valueElementKeySet[count];
 	     				  let subValue = value[subKey];
-	     				  bAFragment.appendChild(populateBankAccountInfo(subValue, count));
+	     				  bAFragment.appendChild(populateAllBankDetails(subValue, count));
 	             	  }
 	        	  }
+	        	  
+	        	  bAParentFrag.appendChild(buildParentViewAllHeader(bAFragment));
 	        	  
 	        	  // Append the fragment to the account picker
 	        	  let accountPickerModal = document.getElementById('accountPickerWrapper');
@@ -541,7 +543,7 @@ $(document).ready(function(){
 	        	  while (accountPickerModal.firstChild) {
 	        		  accountPickerModal.removeChild(accountPickerModal.firstChild);
 	        	  }
-	        	  accountPickerModal.appendChild(bAFragment);
+	        	  accountPickerModal.appendChild(bAParentFrag);
 	          },
 	          error: function(thrownError) {
 	        	  var responseError = JSON.parse(thrownError.responseText);
@@ -553,6 +555,97 @@ $(document).ready(function(){
 	          }
 		});
 	});
+	
+
+	// Build head for view all
+	function populateBAHead(headValue) {
+		let divHead = document.createElement('div');
+		divHead.classList = 'text-left mt-2 font-weight-bold';
+		divHead.innerText = headValue.substring(0,1) + headValue.substring(1).toLowerCase();
+		return divHead;
+		
+	}
+
+	// Populate complete bank account details
+	function populateAllBankDetails(bankAccount, count) {
+		let wrapperRow = document.createElement('div');
+		wrapperRow.classList = 'row bAAllRow';
+		wrapperRow.id = 'bAR-' + count;
+		
+		// If Selected then highlight account
+		if(bankAccount.selectedAccount) {
+			wrapperRow.classList.add('selectedBA');
+		}
+		
+		// Link Icon
+		let wrapperSVG = document.createElement('div');
+		wrapperSVG.classList = 'col-lg-2 py-2 bAAllIcon';
+		
+		if(bankAccount.linked) {
+			syncSVG = cloneElementAndAppend(wrapperSVG, syncSVG);
+		} else {
+			unsyncSVG = cloneElementAndAppend(wrapperSVG, unsyncSVG);
+		}
+		wrapperRow.appendChild(wrapperSVG);
+		
+		// Bank Account Name
+		let bAName = document.createElement('div');
+		bAName.classList = 'col-lg-5 text-left bAAllName py-2';
+		bAName.innerText = bankAccount.bankAccountName;
+		wrapperRow.appendChild(bAName);
+		
+		// Bank Account Balance
+		let bABalance = document.createElement('div');
+		bABalance.classList = 'col-lg-5 text-right font-weight-bold py-2 bAAllAmount';
+		bABalance.innerText = currentCurrencyPreference + formatNumber(bankAccount.accountBalance, currentUser.locale);
+		wrapperRow.appendChild(bABalance);
+		
+		return wrapperRow;
+	}
+	
+	// Build an X with SVG
+	function xMark() {
+		let syncSVGTwo = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+		syncSVGTwo.setAttribute('x','0px');
+		syncSVGTwo.setAttribute('y','0px');
+		syncSVGTwo.setAttribute('width','15');
+		syncSVGTwo.setAttribute('height','15');
+		syncSVGTwo.setAttribute('viewBox','0 0 50 50');
+		
+		let pathElementTwo = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+		pathElementTwo.setAttribute('class', 'xWithRed');
+		pathElementTwo.setAttribute('d','M31.202,25l13.63-20.445c0.204-0.307,0.224-0.701,0.05-1.026S44.369,3,44,3h-7.34c-0.327,0-0.634,0.16-0.821,0.429L25,19 L14.16,3.429C13.973,3.16,13.667,3,13.34,3H6C5.631,3,5.292,3.203,5.118,3.528s-0.154,0.72,0.05,1.026L18.798,25L5.168,45.445 c-0.204,0.307-0.224,0.701-0.05,1.026S5.631,47,6,47h7.34c0.327,0,0.634-0.16,0.821-0.429L25,31l10.84,15.571 C36.027,46.84,36.333,47,36.66,47H44c0.369,0,0.708-0.203,0.882-0.528s0.154-0.72-0.05-1.026L31.202,25z');
+		syncSVGTwo.appendChild(pathElementTwo);
+		
+		return syncSVGTwo;
+	}
+	
+	// Builds the parent header for view all accounts 
+	function buildParentViewAllHeader(bAFragment) {
+		let wrapperDiv = document.createElement('div');
+		
+		let headerDiv = document.createElement('div');
+		headerDiv.classList = 'headVAWrap row';
+		
+		let title = document.createElement('div');
+		title.classList = 'bAHeading text-left pl-3 pr-0 col-lg-7 font-weight-bold';
+		title.innerText = 'All Accounts';
+		headerDiv.appendChild(title);
+		
+		let backToPreview = document.createElement('a');
+		backToPreview.href = '#';
+		backToPreview.classList = 'text-right col-lg-5 pr-3 previewBA text-dynamic-color';
+		backToPreview.innerText = 'preview';
+		headerDiv.appendChild(backToPreview);
+		wrapperDiv.appendChild(headerDiv);
+		
+		let wrapperContent = document.createElement('div');
+		wrapperContent.classList = 'overflowy-auto';
+		wrapperContent.appendChild(bAFragment);
+		wrapperDiv.appendChild(wrapperContent); 
+		
+		return wrapperDiv;
+	}
 });
 
 // Custom Functions to fetch all accounts
@@ -589,6 +682,29 @@ er_a = {
 			populateAccountInfo(bankAccountsInfo);
 		}
 }
+
+//Build a tick icon
+function tickIcon() {
+	let syncSVG = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+	syncSVG.setAttribute('x','0px');
+	syncSVG.setAttribute('y','0px');
+	syncSVG.setAttribute('width','20');
+	syncSVG.setAttribute('height','20');
+	syncSVG.setAttribute('viewBox','0 0 512 512');
+	syncSVG.setAttribute('class', 'tickOuterLayer');
+	
+	let pathElement = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+	pathElement.setAttribute('class','tickPath1');
+	pathElement.setAttribute('d','M424.3,180c-1-1.2-1.5-2.8-1.5-4.3c-14.8-26.1-15.7-58-30.5-84.1c-41.7,23.4-70.2,65.1-97.4,103.1c-16.4,22.9-31.1,46.4-44.6,71.1c-13.6,24.8-26.8,49.9-42,73.8c-2.2,3.4-7.9,5-10.3,0.7c-7.2-13.3-15.3-26.2-24.6-38.2c-8-10.3-17.1-19.5-25.3-29.6c-12.7-15.7-26.3-34.5-43.9-45.4c-6.4,21-13.9,41.8-17.2,63.6c24.6,15.9,43.4,38.9,61.5,61.6c21.2,26.6,43.1,52,66.9,76.3c15.4-20.1,26-43.5,38.8-65.3c15.1-25.7,32.7-49.4,51.4-72.6c18.7-23.2,40.3-43.7,62-63.9c10.2-9.5,22.2-17.3,33.1-26c8.2-6.6,16.2-13.4,23.7-20.7C424.4,180.2,424.4,180.1,424.3,180z');
+	syncSVG.appendChild(pathElement);
+	
+	let pathElement1 = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+	pathElement1.setAttribute('d','M436.2,170.3h-2.8c-16.3-27.7-16.2-62.6-34-89.9c-1.9-3-5.4-3.1-8.3-1.6c-45.6,23.1-76.2,67.7-105.2,108.1c-16.6,23.2-31.6,47-45.4,72c-12.3,22.3-24.1,44.8-37.4,66.6c-6-10.4-12.6-20.4-19.9-29.9c-8.2-10.8-17.8-20.3-26.3-30.8c-15.2-18.7-31.4-40.9-53.7-51.5c-3.7-1.8-7.4,0.5-8.5,4.2c-6.8,23.7-15.8,47-19.5,71.4c0,0.1,0,0.2,0,0.4c-2,2.7-2.2,7.1,1.6,9.4c26.5,15.6,46,40.8,64.9,64.6c22,27.7,45.2,54.1,70.2,79.1c2.3,2.3,6.4,1.8,8.4-0.5c17.2-20.6,28.7-45,41.8-68.2c14.7-25.9,32-50.3,51.1-73.2c19.2-22.9,40-43.7,61.9-64c10.4-9.7,22.6-17.7,33.8-26.6c9-7.2,17.7-14.7,25.9-22.8c2.3-0.5,4.2-2.1,4.6-4.7c0.6-0.6,1.2-1.3,1.8-1.9C445,176.1,441.2,170.3,436.2,170.3z M400.7,201c-10.9,8.7-22.8,16.5-33.1,26c-21.8,20.2-43.4,40.7-62,63.9c-18.7,23.2-36.3,46.8-51.4,72.6c-12.8,21.8-23.4,45.2-38.8,65.3c-23.8-24.2-45.8-49.7-66.9-76.3c-18.1-22.7-37-45.8-61.5-61.6c3.3-21.8,10.8-42.6,17.2-63.6c17.7,10.9,31.2,29.7,43.9,45.4c8.2,10.1,17.3,19.3,25.3,29.6c9.3,12,17.4,24.9,24.6,38.2c2.4,4.4,8.1,2.7,10.3-0.7c15.3-23.9,28.4-49,42-73.8c13.5-24.7,28.2-48.2,44.6-71.1c27.2-38,55.8-79.8,97.4-103.1c14.8,26.1,15.7,58,30.5,84.1c0,1.5,0.5,3.1,1.5,4.3c0,0.1,0.1,0.2,0.1,0.2C416.9,187.6,408.9,194.4,400.7,201z');
+	syncSVG.appendChild(pathElement1);
+	
+	return syncSVG;
+}
+
 
 // Populate bank account info
 function populateAccountInfo(bankAccountsInfo) {
@@ -978,53 +1094,4 @@ function unsyncSVGFc() {
 	syncSVGTwo.appendChild(gElement);
 	
 	return syncSVGTwo;
-}
-
-// Build a tick icon
-function tickIcon() {
-	let syncSVG = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
-	syncSVG.setAttribute('x','0px');
-	syncSVG.setAttribute('y','0px');
-	syncSVG.setAttribute('width','20');
-	syncSVG.setAttribute('height','20');
-	syncSVG.setAttribute('viewBox','0 0 512 512');
-	syncSVG.setAttribute('class', 'tickOuterLayer');
-	
-	let pathElement = document.createElementNS("http://www.w3.org/2000/svg", 'path');
-	pathElement.setAttribute('class','tickPath1');
-	pathElement.setAttribute('d','M424.3,180c-1-1.2-1.5-2.8-1.5-4.3c-14.8-26.1-15.7-58-30.5-84.1c-41.7,23.4-70.2,65.1-97.4,103.1c-16.4,22.9-31.1,46.4-44.6,71.1c-13.6,24.8-26.8,49.9-42,73.8c-2.2,3.4-7.9,5-10.3,0.7c-7.2-13.3-15.3-26.2-24.6-38.2c-8-10.3-17.1-19.5-25.3-29.6c-12.7-15.7-26.3-34.5-43.9-45.4c-6.4,21-13.9,41.8-17.2,63.6c24.6,15.9,43.4,38.9,61.5,61.6c21.2,26.6,43.1,52,66.9,76.3c15.4-20.1,26-43.5,38.8-65.3c15.1-25.7,32.7-49.4,51.4-72.6c18.7-23.2,40.3-43.7,62-63.9c10.2-9.5,22.2-17.3,33.1-26c8.2-6.6,16.2-13.4,23.7-20.7C424.4,180.2,424.4,180.1,424.3,180z');
-	syncSVG.appendChild(pathElement);
-	
-	let pathElement1 = document.createElementNS("http://www.w3.org/2000/svg", 'path');
-	pathElement1.setAttribute('d','M436.2,170.3h-2.8c-16.3-27.7-16.2-62.6-34-89.9c-1.9-3-5.4-3.1-8.3-1.6c-45.6,23.1-76.2,67.7-105.2,108.1c-16.6,23.2-31.6,47-45.4,72c-12.3,22.3-24.1,44.8-37.4,66.6c-6-10.4-12.6-20.4-19.9-29.9c-8.2-10.8-17.8-20.3-26.3-30.8c-15.2-18.7-31.4-40.9-53.7-51.5c-3.7-1.8-7.4,0.5-8.5,4.2c-6.8,23.7-15.8,47-19.5,71.4c0,0.1,0,0.2,0,0.4c-2,2.7-2.2,7.1,1.6,9.4c26.5,15.6,46,40.8,64.9,64.6c22,27.7,45.2,54.1,70.2,79.1c2.3,2.3,6.4,1.8,8.4-0.5c17.2-20.6,28.7-45,41.8-68.2c14.7-25.9,32-50.3,51.1-73.2c19.2-22.9,40-43.7,61.9-64c10.4-9.7,22.6-17.7,33.8-26.6c9-7.2,17.7-14.7,25.9-22.8c2.3-0.5,4.2-2.1,4.6-4.7c0.6-0.6,1.2-1.3,1.8-1.9C445,176.1,441.2,170.3,436.2,170.3z M400.7,201c-10.9,8.7-22.8,16.5-33.1,26c-21.8,20.2-43.4,40.7-62,63.9c-18.7,23.2-36.3,46.8-51.4,72.6c-12.8,21.8-23.4,45.2-38.8,65.3c-23.8-24.2-45.8-49.7-66.9-76.3c-18.1-22.7-37-45.8-61.5-61.6c3.3-21.8,10.8-42.6,17.2-63.6c17.7,10.9,31.2,29.7,43.9,45.4c8.2,10.1,17.3,19.3,25.3,29.6c9.3,12,17.4,24.9,24.6,38.2c2.4,4.4,8.1,2.7,10.3-0.7c15.3-23.9,28.4-49,42-73.8c13.5-24.7,28.2-48.2,44.6-71.1c27.2-38,55.8-79.8,97.4-103.1c14.8,26.1,15.7,58,30.5,84.1c0,1.5,0.5,3.1,1.5,4.3c0,0.1,0.1,0.2,0.1,0.2C416.9,187.6,408.9,194.4,400.7,201z');
-	syncSVG.appendChild(pathElement1);
-	
-	return syncSVG;
-}
-
-// Build an X with SVG
-function xMark() {
-	let syncSVGTwo = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
-	syncSVGTwo.setAttribute('x','0px');
-	syncSVGTwo.setAttribute('y','0px');
-	syncSVGTwo.setAttribute('width','15');
-	syncSVGTwo.setAttribute('height','15');
-	syncSVGTwo.setAttribute('viewBox','0 0 50 50');
-	
-	let pathElementTwo = document.createElementNS("http://www.w3.org/2000/svg", 'path');
-	pathElementTwo.setAttribute('class', 'xWithRed');
-	pathElementTwo.setAttribute('d','M31.202,25l13.63-20.445c0.204-0.307,0.224-0.701,0.05-1.026S44.369,3,44,3h-7.34c-0.327,0-0.634,0.16-0.821,0.429L25,19 L14.16,3.429C13.973,3.16,13.667,3,13.34,3H6C5.631,3,5.292,3.203,5.118,3.528s-0.154,0.72,0.05,1.026L18.798,25L5.168,45.445 c-0.204,0.307-0.224,0.701-0.05,1.026S5.631,47,6,47h7.34c0.327,0,0.634-0.16,0.821-0.429L25,31l10.84,15.571 C36.027,46.84,36.333,47,36.66,47H44c0.369,0,0.708-0.203,0.882-0.528s0.154-0.72-0.05-1.026L31.202,25z');
-	syncSVGTwo.appendChild(pathElementTwo);
-	
-	return syncSVGTwo;
-}
-
-// Build head for view all
-function populateBAHead(headValue) {
-	let divHead = document.createElement('div');
-	divHead.classList = '';
-	divHead.innerText = headValue;
-	
-	return divHead;
-	
 }
